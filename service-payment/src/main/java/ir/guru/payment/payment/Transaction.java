ee9fe1b47b8a9115a95c1821d48fcdb966e52f3a
iEAmi <Eahmad.edge@gmail.com>
2025-11-04 21:56:21 +0330

    Formate code

--- service-payment/src/main/java/ir/guru/payment/payment/Transaction.java
+++ service-payment/src/main/java/ir/guru/payment/payment/Transaction.java
@@ -11,7 +11,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.lang.Nullable;
 
-
 @Getter(AccessLevel.PACKAGE)
 @Entity(name = "Campaign")
 @Table(name = "campaigns")
