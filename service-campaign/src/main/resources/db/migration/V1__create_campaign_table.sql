c7eb2cadcc64e33e9f16f5a5b985b7fd18e215b5
iEAmi <Eahmad.edge@gmail.com>
2025-11-04 20:00:00 +0330

    Fix migrations

--- service-campaign/src/main/resources/db/migration/V1__create_campaign_table.sql
+++ service-campaign/src/main/resources/db/migration/V1__create_campaign_table.sql
@@ -2,13 +2,16 @@ CREATE SEQUENCE IF NOT EXISTS campaign_sequence START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE IF NOT EXISTS campaigns
 (
-    id BIGINT PRIMARY KEY DEFAULT nextval('campaign_sequence'),
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    target_amount_rials BIGINT NOT NULL,
-    current_amount_rials BIGINT NOT NULL DEFAULT 0,
-    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    amount_rials_reached_at TIMESTAMP WITHOUT TIME ZONE,
-    version BIGINT
+    id                             BIGINT PRIMARY KEY    DEFAULT nextval('campaign_sequence'),
+
+    name                           VARCHAR(255) NOT NULL,
+    description                    TEXT,
+    current_amount_rials           BIGINT       NOT NULL DEFAULT 0,
+
+    target_amount_rials            BIGINT       NOT NULL,
+    target_amount_rials_reached_at TIMESTAMPTZ,
+
+    created_at                     TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    updated_at                     TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    version                        BIGINT       NOT NULL DEFAULT 0
 );
--- service-payment/src/main/resources/db/migration/V1__create_payment_tables.sql
+++ service-payment/src/main/resources/db/migration/V1__create_payment_tables.sql
@@ -1,23 +1,15 @@
-CREATE SEQUENCE IF NOT EXISTS donation_sequence START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE IF NOT EXISTS transaction_sequence START WITH 1 INCREMENT BY 1;
-
-CREATE TABLE IF NOT EXISTS donations
-(
-    id BIGINT PRIMARY KEY DEFAULT nextval('donation_sequence'),
-    amount_rials BIGINT NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    version BIGINT NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS transactions
 (
-    id BIGINT PRIMARY KEY DEFAULT nextval('transaction_sequence'),
-    amount_rials BIGINT NOT NULL,
-    username VARCHAR(255) NOT NULL,
+    id                BIGINT PRIMARY KEY    DEFAULT nextval('transaction_sequence'),
+
+    amount_rials      BIGINT       NOT NULL,
+    username          VARCHAR(255) NOT NULL,
     unique_identifier VARCHAR(255) NOT NULL UNIQUE,
-    description TEXT,
-    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    version BIGINT NOT NULL
+    description       TEXT,
+
+    created_at        TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    updated_at        TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    version           BIGINT       NOT NULL DEFAULT 0
 );
